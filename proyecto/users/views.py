62c9316dbc480e6c4911dc722f61157669a1076a
Andres Rojas <andresrojas1999@gmail.com>
2023-04-03 19:12:49 -0500

    perfiles y usuarios creados

--- proyecto/users/views.py
+++ proyecto/users/views.py
@@ -1,19 +1,26 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm 
 from django.contrib import messages
+from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 
 def register(request):
 
     if request.method == "POST":
-        form = UserCreationForm
+        form = UserRegisterForm
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get("username")
-            messages.success(request, f"la cuenta fue creada para {username}")
+            messages.success(request, f"la cuenta fue creada para {username} inicia sesion  ahora")
 
-            return redirect ("blog-home")
+            return redirect ("login")
     else:
-        form = UserCreationForm
+        form = UserRegisterForm
 
-    return render(request, "users/register,html", {"form":form})
+    return render(request, "users/register.html", {"form":form})
+
+@login_required
+def profile(request):
+
+    return render(request, 'users/profile.html')
